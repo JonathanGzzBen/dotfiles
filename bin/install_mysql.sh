30fed00af731166f9bdffe09d6ec09297e9a9807
JonathanGzzBen <jonathangzzben@gmail.com>
2020-07-18 13:20:33 -0500

    Remove Dotbot

--- bin/install_mysql.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo apt-get update
-sudo apt-get install mysql-server
-sudo mysql_secure_installation utility
-sudo systemctl start mysql
-sudo systemctl enable mysql
-
--- bin/powerlevel10k_install.sh
+++ bin/powerlevel10k_install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Instal oh-my-zsh
+$ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 # Open browser to install recommended fonts for powerlevel10k

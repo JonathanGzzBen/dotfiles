f32668cbac35f525efeb85a08358d5dea8ec03d7
Jonathan Gzz <jonark@pop-os.localdomain>
2020-11-20 14:07:23 -0600

    Add ffmpeg to bootstrap script

--- bootstrap.exclude.sh
+++ bootstrap.exclude.sh
@@ -33,6 +33,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install yarn -y
 sudo apt install nodejs npm -y
+sudo apt install ffmpeg -y
 install_powerlevel10k
 link
 

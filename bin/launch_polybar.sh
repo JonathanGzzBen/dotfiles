7ab743e1b4afb44e7a4cb41d84a01b7ba26c56d8
JonathanGzzBen <jonathangzzben@gmail.com>
2021-07-30 16:44:28 -0500

    Add launch_polybar script

---
+++ bin/launch_polybar.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+killall -q polybar
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload mybar &
+	done
+else
+	polybar --reload mybar &
+fi
+

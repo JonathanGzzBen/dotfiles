1ded939d27aa9f16489220b9dd8415ee57ba76a3
JonathanGzzBen <jonathangzzben@gmail.com>
2022-04-12 09:50:55 -0500

    Add minimal ubuntu setup

--- README.md
+++ README.md
@@ -1,28 +1,10 @@
 # JonathanGzzBen dotfiles
 
-In Arch-based distros, run `bootstrap.arch.exclude.sh` to install personal software and symlink configuration files.
-
-## Instructions
-
-[Installation instructions](arch-installation-instructions.exclude.md)
-
-[Setup instructions](arch-setup-instructions.exclude.md)
+This setup was built for a simple kubuntu setup. Run `bootstrap.ubuntu.exclude.sh` to copy config files and install my commonly used software.
 
 ## Software used in this setup
 
-- LightDM
-- i3
-- ULauncher
-- Polybar
-- feh
-- alacritty
-- zsh
-- neovim
-- Thunar
-- lf
-- dunst
-- ncspot
-
-## Where to place files
-
-Background images should be placed on `/usr/share/backgrounds` to be loaded automatically by `feh` when running `i3`.
+- fish
+- tilix
+- nvim
+- ranger
---
+++ bootstrap.ubuntu.exclude.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Make symlinks
+link() {
+    echo "Symlink dotfiles to home directory? [y/N]"
+    read resp
+    if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+		ln -svf .vimrc $HOME/
+        echo "Symlinking complete"
+    else
+        echo "Symlinking canceled"
+        return 1
+    fi
+}
+
+link_configurations() {
+    echo "Symlink configuration files? [y/N]"
+    read resp
+    if [ "resp" = 'y' -o "$resp" = 'y' ] ; then
+        for file in $(ls .config -A) ; do
+	    config_directory="$HOME/.config"
+	    full_file_path=$(readlink -f $file)
+	    prefix="/home/jonark/dotfiles/"
+	    link_path=${full_file_path#$prefix}
+	    echo "$PWD/.config/$file" "$HOME/.config/$link_path"
+	    ln -sdvf "$PWD/.config/$file" "$HOME/.config/"
+        done
+	echo "Symlinking configurations complete"
+    else
+        echo "Symlinking configurations canceled"
+    fi
+}
+
+install_fish_agnoster() {
+    echo "Install fish, omf and agnoster theme? [Y/n]"
+    read resp
+    if [ "$resp" != 'n' -o "$resp" != 'N' ] ; then
+		sudo apt install fish
+		curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+		omf install agnoster
+		chsh -s $(which fish)
+    fi
+	echo "fish set as default shell"
+}
+
+install_fish_agnoster
+link
+link_configurations
+
+sudo apt install tilix python3-pip ranger
